4d4f8fe8b92f0c403334e7738580e9baf8d5b2e7
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-03-23 00:25:58 -0400

    new file forr square and multiple algo

---
+++ square-and-multiply.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class square_and_multiply {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ square_and_multiply.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class square-and-multiply {
+    public static void main(String[] args) {
+        
+    }
+}
