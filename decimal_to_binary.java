b68a252146a0a910ba62c63c257a438147de1fe9
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-03-22 23:59:45 -0400

    count bits function working fine

--- decimal_to_binary.java
+++ decimal_to_binary.java
@@ -1,5 +1,14 @@
+import java.util.*;
+import java.io.*;
+
 public class decimal_to_binary {
     public static void main(String[] args) {
-        
+        System.out.println(countBits(2398));
+    }
+
+    // function to count the number of bits of a decimal
+    static int countBits(int num_in)
+    { 
+        return (int)(Math.log(num_in) / Math.log(2) + 1);
     }
 }
